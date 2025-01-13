fba1490c7e7d362c0327bbe324ced2fa9f912cb1
Marcello Aires <00043942@randon.com.br>
2025-01-13 19:12:52 -0300

    multinomial num_samples

--- cifar/models.py
+++ cifar/models.py
@@ -850,7 +850,7 @@ class RLFeedforwardGateI(nn.Module):
         softmax = self.prob_layer(x)
 
         if self.training:
-            action = softmax.multinomial()
+            action = softmax.multinomial(num_samples=1)
             self.saved_action = action
         else:
             action = (softmax[:, 1] > 0.5).float()
@@ -891,7 +891,7 @@ class RLFeedforwardGateII(nn.Module):
         softmax = self.prob_layer(x)
 
         if self.training:
-            action = softmax.multinomial()
+            action = softmax.multinomial(num_samples=1)
             self.saved_action = action
         else:
             action = (softmax[:, 1] > 0.5).float()
@@ -1034,7 +1034,7 @@ class ResNetFeedForwardRL(nn.Module):
 
         if reinforce:  # for pure RL
             softmax = self.softmax(x)
-            action = softmax.multinomial()
+            action = softmax.multinomial(num_samples=1)
             self.saved_actions.append(action)
 
         return x, masks, gprobs
@@ -1135,7 +1135,7 @@ class RNNGatePolicy(nn.Module):
             proj = self.proj(out.squeeze())
             prob = self.prob(proj)
             bi_prob = torch.cat([1 - prob, prob], dim=1)
-            action = bi_prob.multinomial()
+            action = bi_prob.multinomial(num_samples=1)
             self.saved_actions.append(action)
         else:
             proj = self.proj(out.squeeze())
@@ -1274,7 +1274,7 @@ class ResNetRecurrentGateRL(nn.Module):
 
             # x = self.fc(x)
             # softmax = self.softmax(x)
-            # pred = softmax.multinomial()
+            # pred = softmax.multinomial(num_samples=1)
         else:
             x = self.fc(x)
             pred = x.max(1)[1]
